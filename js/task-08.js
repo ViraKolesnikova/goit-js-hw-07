146c112b10d76d6fa77fb5559ae824eba2239b69
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-09-03 12:33:58 +0300

    homework 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -3,24 +3,37 @@ const createBtnEl = document.querySelector('[data-action = "render"]');
 const removeBtnEl = document.querySelector('[data-action = "destroy"]');
 const boxesContainerEl = document.querySelector('#boxes');
 
-amountInputEl.addEventListener('blur', getAmountValue);
+
 createBtnEl.addEventListener('click', createBoxes);
-// removeBtnEl.addEventListener('click', removeBoxes);
+removeBtnEl.addEventListener('click', removeBoxes);
+
+
+function createBoxes() {
+  const boxes = [];
+  
+  for (let i = 1; i <= getAmountValue(); i += 1) {
+    const newBox = document.createElement('div');
+    newBox.style.width = '30px';
+    newBox.style.height = '30px';
+    newBox.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
+    boxes.push(newBox);
+  }  
+  
+  for (let i = 1; i < boxes.length; i += 1){
+    boxes[i].style.height = Number.parseInt(boxes[0].style.height) + 10 * i + 'px';
+    boxes[i].style.width = Number.parseInt(boxes[0].style.width) + 10 * i + 'px';
+  };
+
+  boxesContainerEl.append(...boxes);
+  amountInputEl.value = '';
+}
 
 function getAmountValue() {
-  let result = amountInputEl.value
+ let result = amountInputEl.value;
   return result;
 }
 
-function createBoxes(amount) {
-  const boxes = [];
-  console.log(boxes);
-  while (boxes.length !== amount) {
-    boxes.push(document.createElement('div'));
-  }
-  console.log(boxes);
+function removeBoxes() {
+  boxesContainerEl.innerHTML = '';
+  amountInputEl.value = '';
 }
-
-// function removeBoxes() {
-//   boxesContainerEl.innerHTML = '';
-// }
